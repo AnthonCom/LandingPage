cda08ffd42e6d0d4662069561cf445eaa9ed49c4
AnthoCom <anthony_cotterell@hotmail.co.uk>
2024-08-23 11:27:56 +0100

    Initial deployment

---
+++ script.js
@@ -0,0 +1,7 @@
+// Setting a timer to hide the hyperlink button while the text "types"
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('myButton').style.display = 'block';
+    }, 9000);
+}; 
